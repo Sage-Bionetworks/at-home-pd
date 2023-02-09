9f404bfe83eaa7ba39a6922160d63d0b1cb59260
Phil Snyder <subfivemile@gmail.com>
2023-02-08 18:05:22 -0800

    Update field mapping with annual_survey fields

--- to_dmr/helpers/field_mapping_transformer.R
+++ to_dmr/helpers/field_mapping_transformer.R
@@ -1,21 +1,15 @@
 #' Output ctcc_dmr_fields.csv in a tidy format
+#' The working directory must be the same as this script's directory
+#' in order for this script to execute properly.
 
 library(dplyr)
 library(tidyr)
 library(stringr)
 
-read_args <- function() {
-  parser <- argparse::ArgumentParser()
-  parser$add_argument("--input", default="syn23593083")
-  parser$add_argument("--output", default="syn16809549")
-  args <- parser$parse_args()
-  return(args)
-}
-
 format_input <- function(ctcc_dmr_fields) {
   ctcc_dmr_fields %>%
     select(dmr_variable, at_home_pd_baseline, at_home_pd_m12, at_home_pd_m24,
-           super_pd_baseline, super_pd_on, super_pd_off) %>%
+           at_home_pd_m36, super_pd_baseline, super_pd_on, super_pd_off) %>%
     pivot_longer(!dmr_variable, names_to="cohort_visit", values_to="clinical_variable") %>%
     mutate(cohort = ifelse(
               str_detect(cohort_visit, "at_home_pd"), "at-home-pd", "super-pd"),
@@ -24,37 +18,21 @@ format_input <- function(ctcc_dmr_fields) {
              visit == "baseline" ~ "Baseline",
              visit == "m12" ~ "12 months",
              visit == "m24" ~ "24 months",
+             visit == "m36" ~ "36 months",
              visit == "on" ~ "Physician_ON",
              visit == "off" ~ "Physician_OFF")) %>%
     select(dmr_variable, cohort, visit, clinical_variable)
 }
 
-read_from_synapse <- function(synapse_id) {
-  f <- synapser::synGet(synapse_id)
-  df <- readr::read_csv(f$path)
-  return(df)
-}
-
-store_to_synapse <- function(formatted_ctcc_dmr_fields, synapse_parent) {
-  fname <- "tidy_ctcc_dmr_fields.csv"
-  readr::write_csv(formatted_ctcc_dmr_fields, fname)
-  f <- synapser::File(fname, synapse_parent)
-  synapser::synStore(f)
-  unlink(fname)
-}
-
 main <- function() {
-  args <- read_args()
-  synapser::synLogin()
-  ctcc_dmr_fields <- read_from_synapse(args$input)
-  dmr_dic <- read_from_synapse("syn24171997") %>%
+  ctcc_dmr_fields <- readr::read_csv("resources/ctcc_dmr_fields.csv")
+  dmr_dic <- readr::read_csv("resources/pdbp_complete_data_dictionary.csv") %>%
     select(dmr_variable = field_name, form_name)
   formatted_ctcc_dmr_fields <- format_input(ctcc_dmr_fields) %>%
     inner_join(dmr_dic, by="dmr_variable") %>%
     select(dmr_variable, form_name, dplyr::everything())
-  store_to_synapse(
-    formatted_ctcc_dmr_fields = formatted_ctcc_dmr_fields,
-    synapse_parent = args$output)
+  fname <- "../resources/dmr_to_clinical_field_mapping.csv"
+  readr::write_csv(formatted_ctcc_dmr_fields, fname)
 }
 
 main()
